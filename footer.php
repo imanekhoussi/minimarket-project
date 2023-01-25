d27186f36c8b8e8dfe13fb747a8cbf1387b2fe07
imanekhoussi <116308913+imanekhoussi@users.noreply.github.com>
2023-01-25 18:31:05 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,57 @@
+<section class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3">
+                <h3><i class="fas fa-link"></i>Quick Links</h3>
+                <ul class="list-unstyled">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Review</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <h3> <i class="fas fa-ellipsis-h"></i> extra links</h3>
+                <ul class="list-unstyled">
+                    <li><a href="#">My Account</a></li>
+                    <li><a href="#">My Order</a></li>
+                    <li><a href="#">My Favorite</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <h3><i class="fas fa-map-marker-alt"></i> Locations</h3>
+                <ul class="list-unstyled">
+                    <li><a href="#">TANGER</a></li>
+                    <li><a href="#">RABAT</a></li>
+                    <li><a href="#">AGADIR</a></li>
+                    <li><a href="#">KENITRA</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+            <h3> <i class="fas fa-envelope"></i> contact info</h3>
+                <ul class="list-unstyled">
+                    <li><a href="tel:+212-661234532">+212-661234532</a></li>
+                    <li><a href="mailto:example@gmail.com">example@gmail.com</a></li>
+                </ul>
+                <img src="payment.png" alt="Payment methods accepted" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+
+<style>
+    .footer h3 {
+    color: #805f64;
+}
+.footer a {
+    color: black;
+}
+
+
+    .footer{
+        background:#f7dfb5;
+        max-width: 100%;
+        
+    }
+</style>
